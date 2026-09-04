bdb4cb43a4cc3a26b05958e57e88de6a50f4ec5a
d77167635 <d77167635@gmail.com>
2026-09-04 10:53:46 -0400

    Apply premium Iris workspace surface

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import "./styles.css";
 import "./premium-dashboard.css";
 import "./interaction-fix.css";
 import "./iris-experience.css";
+import "./iris-premium.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
