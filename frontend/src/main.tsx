9ba9361c8f2762012c1a225859811260e339f453
d77167635 <d77167635@gmail.com>
2026-09-04 22:54:16 -0400

    fix: remove legacy global Iris and Plaid style stacks

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,15 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import "./interaction-fix.css";
-import "./iris-experience.css";
-import "./iris-premium.css";
-import "./plaid-command.css";
-import "./iris-spatial-upgrade.css";
-import "./iris-screen-system.css";
-import "./iris-screen-modes.css";
-import "./iris-accessibility.css";
-import "./iris-assistant-accessibility.css";
-import "./iris-visibility.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
